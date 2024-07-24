02cf5c48635f26f962fc8d9222b755b2f7c5f925
Programmac1 <148036603+Programmac1@users.noreply.github.com>
2024-07-24 20:43:02 +0100

    header component added

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './styles/header.css';
+
+const Header = () => {
+    return (
+        <header className="title">
+            <h1>Brandon's react website
+            </h1>
+        </header>
+    )
+}
+
+export default Header;
